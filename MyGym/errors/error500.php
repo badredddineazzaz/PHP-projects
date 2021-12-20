da2c504517e7bf4cd5535a91a462b736fc8b1be1
badredddineazzaz <84666903+badredddineazzaz@users.noreply.github.com>
2021-12-20 17:53:15 +0100

    Add files via upload

---
+++ MyGym/errors/error500.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: rgb(236, 236, 236);
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            color: #192958;
+        }
+    </style>
+</head>
+<body>
+    <img width="500px" src="../images/500error.svg" alt="">
+    <h1>Sorry, there is an error with the server.</h1>
+</body>
+</html>
